746260821aa20793f7e3ee008617ea8648724042
BayramBehbudov <bayram.behbudov@gmail.com>
2024-05-31 13:14:21 +0400

    update like

--- pages/likedProdPage.jsx
+++ pages/likedProdPage.jsx
@@ -1,12 +1,20 @@
-import React from "react";
 import setSelectedItem from "../changedElement.js";
 import ProductItem from "../components/productCard.jsx";
 import style from "../style/components/likeIcon.module.css";
+import Button from "../components/btnWithStars.jsx";
 
 function likedProdPage() {
   const { likedProducts } = setSelectedItem();
+
   if (likedProducts.length == 0) {
-    return <div className={style.container}>liked products empty</div>;
+    return (
+      <div className={style.container}>
+        <div className={style.context}>
+          <h1>You haven't liked any products yet</h1>
+          <Button />
+        </div>
+      </div>
+    );
   } else {
     return (
       <div className={style.container}>
